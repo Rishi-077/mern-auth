28a51fd05c514c2d6040ef82c813497a844d9d60
Rishi-077 <rishikesan070@gmail.com>
2023-11-18 16:56:28 +0530

    changes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <div>App</div>
 
+      <div>rishi</div>
+    </>
+  );
+}
