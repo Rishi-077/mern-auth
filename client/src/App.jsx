a8642e2f161e183a80ca2db8eb99092fab3e8881
Rishi-077 <rishikesan070@gmail.com>
2023-11-18 17:08:31 +0530

    route added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import Signin from "./pages/Signin";
 
 export default function App() {
   return (
     <>
-      <div>App</div>
-
-      <div>rishi</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
