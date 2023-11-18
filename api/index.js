9739da26230dfe82bd9580bfa3d6c02b370c95d2
Rishi-077 <rishikesan070@gmail.com>
2023-11-18 20:27:01 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connect to the mongodb !!!");
+  })
+  .catch((err) => {
+    console.log("Rishi", err, process.env.MONGO);
+  });
+  
 const port = 3000;
 app.listen(port, () => {
   console.log(`Listening Port on ${port}!!!`);
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  console.log('rishi');
   return (
     <>
       <div className="bg-slate-200">
