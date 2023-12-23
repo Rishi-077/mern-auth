27aeb8e5541a5b7395f302a127c68e14459401a3
Rishi-077 <rishikesan070@gmail.com>
2023-12-23 18:10:32 +0530

    private route added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import Signin from "./pages/Signin";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
@@ -15,9 +16,11 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/profile" element={<Profile />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<Signin />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
-  console.log('rishi');
+  const currentUser = useSelector((state) => state.user);
   return (
     <>
       <div className="bg-slate-200">
@@ -17,8 +18,16 @@ export default function Header() {
             <Link to={"/about"}>
               <li>About</li>
             </Link>
-            <Link to={"/signin"}>
-              <li>Sign In</li>
+            <Link to={"/profile"}>
+              {currentUser.currentUser ? (
+                <img
+                  src={currentUser.currentUser.profilePicture}
+                  alt="img"
+                  className="h-7 w-7 rounded-full object-cover"
+                />
+              ) : (
+                <li>Sign In</li>
+              )}
             </Link>
           </ul>
         </div>
