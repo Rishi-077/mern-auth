c04b7c389b6e2078becba88e742841e38685afd6
Rishi-077 <rishikesan070@gmail.com>
2024-03-03 01:54:10 +0530

    update user functionality completed

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,13 +1,12 @@
-import { Jwt } from "jsonwebtoken";
-import { errorHandler } from "./error";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookie.access_token;
-  if (!token) {
-    return next(errorHandler(401, 'You are not authenticated!'))
-  }
-  Jwt.verify(token, process.env.JWT_TOKEN, (err, user) => {
-    if (err) return next(errorHandler(403, 'Token is not valid!'))
+  const token = req.cookies.access_token;
+  if (!token) return next(errorHandler(401, "You are not authenticated!"));
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return next(errorHandler(403, "Token is not valid!"));
 
     req.user = user;
     next();
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   getDownloadURL,
   getStorage,
@@ -7,14 +7,21 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
+import {
+  updateUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+} from "../redux/user/userSlice";
 
 export default function Profile() {
+  const dispatch = useDispatch();
   const fileRef = useRef(null);
   const [image, setImage] = useState(undefined);
   const [uploadPercentage, setUploadPercentage] = useState(0);
   const [imageError, setImageError] = useState(false);
   const [formData, setFormData] = useState({});
-  const currentUser = useSelector((state) => state.user.currentUser);
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
 
   const handleImageUpload = async (image) => {
     const storage = getStorage(app);
@@ -46,15 +53,40 @@ export default function Profile() {
     }
   }, [image]);
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error));
+    }
+  };
+
   return (
     <>
       <div className="p-3 max-w-lg mx-auto">
         <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-        <form action="" className="flex flex-col gap-4">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
           {/*  
-
         firebase storage rules
-
           allow read;
       allow write: if
       request.resource.size < 2 * 1024 * 1024 &&
@@ -94,19 +126,22 @@ export default function Profile() {
             id="username"
             placeholder="username"
             className="bg-slate-100 rounded-lg  p-3"
+            onChange={handleChange}
           />
           <input
             defaultValue={currentUser.email}
-            type="eamil"
-            id="eamil"
+            type="email"
+            id="email"
             placeholder="Email"
             className="bg-slate-100 rounded-lg  p-3"
+            onChange={handleChange}
           />
           <input
             type="password"
             id="password"
             placeholder="Password"
             className="bg-slate-100 rounded-lg  p-3"
+            onChange={handleChange}
           />
           <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity:95  disabled:opacity-80">
             Update
@@ -116,6 +151,10 @@ export default function Profile() {
           <span className="text-red-700 cursor-pointer">Delete Account</span>
           <span className="text-red-700 cursor-pointer">Sign out</span>
         </div>
+        <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
+        <p className="text-green-700 mt-5">
+          {updateSuccess && "User is updated successfully!"}
+        </p>
       </div>
     </>
   );
